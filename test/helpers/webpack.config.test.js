55b3b2d9a230e9b66b2fdbfde50fcdfd7bec0f55
Erik Ekberg <eekberg@rei.com>
2019-02-28 17:34:18 -0800

    Getting closer to useing mocah-webpack

--- test/helpers/webpack.config.test.js
+++ test/helpers/webpack.config.test.js
@@ -1,14 +1,6 @@
-const path = require('path');
-const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 
-module.exports = {
-	entry: './src/index.vue',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist/',
-		filename: 'build.js',
-	},
+const config = {
 	plugins: [
 		new VueLoaderPlugin(),
 	],
@@ -51,30 +43,6 @@ module.exports = {
 			vue$: 'vue/dist/vue.esm.js',
 		},
 	},
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true,
-	},
-	devtool: '#eval-source-map',
 };
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = '#source-map';
-	// http://vue-loader.vuejs.org/en/workflow/production.html
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"',
-			},
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false,
-			},
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true,
-		}),
-	]);
-}
+module.exports = config;
